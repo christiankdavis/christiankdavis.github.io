aaa8c7c592cad5abf6a608cc6995b55555ac329c
Christian Davis <cdavis20@stanford.edu>
2018-11-28 23:16:23 -0800

    a lot more stuff is working. menu works the way i want and the background images resize with the sections.

--- scripts.js
+++ scripts.js
@@ -1,35 +1,33 @@
 function fixBackgroundSize() {
-	var sections = getElementsByClassName("section");
-	var images = getElementsByClassName("background_img");
-	images[0].style.height = "50px"
+	var sections = document.getElementsByClassName("section");
+	var images = document.getElementsByClassName("background_img");
+	var divHeight = sections[0].clientHeight;
 
 	var i;
-	for(i = 0; i < sections.length - 1 && i < images.length - 1; i++) {
-		images[i].style.height = "50px"
-		if(sections[i].style.height > images[i].style.height) {
-			images[i].style.height = sections[i].style.height;
+	for(i = 0; i < images.length - 1; i++) {
+		if(divHeight > images[i].clientHeight) {
+			images[i].style.height = sections[i].clientHeight + "px";
 		}
 	}
-
 }
 
-function navHighlight(elem, home, active) {
-    var url = location.href.split("/"); //replace string with location.href
-	var navLinks = document.getElementsByTagName(elem)[0].getElementsByTagName("a");
-	//naturally you could use something other than the <nav> element
+function navHighlight(currentPage) {
+	//var url = window.location.href.split("#"); //replace string with location.href
+	var listItems = document.getElementById("navBar").getElementsByTagName("li");
 	var i=0;
-	var currentPage = url[url.length - 1];
-	for(i;i<navLinks.length;i++){
-  		var lb = navLinks[i].href.split("/");
+	//var currentPage = url[url.length - 1];
+	for(i;i<listItems.length;i++){
+  		var lb = listItems[i].getElementsByTagName("a")[0].href.split("#");
   		if(lb[lb.length-1] == currentPage) {
-   			navLinks[i].className = "current";
-
+   			listItems[i].className = "current";
+  		}else {
+  			listItems[i].className = "notCurrent";
   		}
   	}
 }
 
 
-navHighlight('#navBar', 'index.html#main', 'current'); /* menu link selector, home page, highlight class */
+//navHighlight('#navBar', 'index.html#main', 'current'); /* menu link selector, home page, highlight class */
 
-
+window.onload = fixBackgroundSize;
 window.onresize = fixBackgroundSize;
